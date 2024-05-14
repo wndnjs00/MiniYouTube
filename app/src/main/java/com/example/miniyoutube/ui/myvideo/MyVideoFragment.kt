9dc51b9d8536e5838e1f45ff1cea83e6b2bc0a78
Choi YoonChan <choyc9239@gmail.com>
2024-05-14 10:23:43 +0900

    [feat/#6] MyVideoFragment 불필요코드 삭제

--- app/src/main/java/com/example/miniyoutube/ui/myvideo/MyVideoFragment.kt
+++ app/src/main/java/com/example/miniyoutube/ui/myvideo/MyVideoFragment.kt
@@ -24,11 +24,6 @@ class MyVideoFragment : Fragment() {
         })
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
