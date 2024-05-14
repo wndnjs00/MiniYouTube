c0e268356605d39c6e0b183c4100a3b9e49144ed
lany159 <tnfl2644@gmail.com>
2024-05-14 11:05:45 +0900

    [feat] HomeFragment UI 작성_ setSpinner 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,14 @@
-import java.util.Properties
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.ksp)
     id ("kotlin-parcelize")
-    kotlin("kapt")
 }
-val properties = Properties().apply {
-    load(project.rootProject.file("local.properties").inputStream())
+
+fun getApiKey(propertyKey: String): String {
+    return gradleLocalProperties(rootDir,providers).getProperty(propertyKey)
 }
 
 android {
@@ -28,7 +28,11 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField("String", "GOOGLE_API_KEY", properties.getProperty("GOOGLE_API_KEY"))
+        buildConfigField("String", "GOOGLE_API_KEY", getApiKey("GOOGLE_API_KEY"))
+    }
+
+    packaging {
+        resources.excludes += "META-INF/DEPENDENCIES"
     }
 
     buildTypes {
@@ -66,8 +70,29 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation (libs.google.api.services.youtube)
+    implementation (libs.google.api.services.youtube) {
+        exclude("org.apache.httpcomponents")
+    }
     implementation (libs.google.http.client.android)
-    implementation (libs.google.api.client.android)
+    implementation (libs.google.api.client.android) {
+        exclude("org.apache.httpcomponents")
+    }
     implementation (libs.google.api.client.gson)
+
+    implementation ("androidx.room:room-runtime:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    implementation ("com.squareup.okhttp3:okhttp:4.6.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.6.0")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
