b9f5f10bd85b5e08c6ca30ae8fe9c6a75383e101
kory0115 <kory01167@gmail.com>
2024-05-14 09:42:44 +0900

    add: hilt module setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,21 @@ dependencies {
         exclude("org.apache.httpcomponents")
     }
     implementation (libs.google.api.client.gson)
+
+    implementation ("androidx.room:room-runtime:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    implementation ("com.squareup.okhttp3:okhttp:4.6.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.6.0")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
