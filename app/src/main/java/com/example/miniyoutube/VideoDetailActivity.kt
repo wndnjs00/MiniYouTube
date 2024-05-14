ffe1d2f2a3e3152a75b570e8f7b2a5b3f496a094
wndnjs00 <juwonjeon852@gmail.com>
2024-05-14 11:35:19 +0900

    design : MainActivity, VideoDetailActivity 디자인

--- app/src/main/java/com/example/miniyoutube/VideoDetailActivity.kt
+++ app/src/main/java/com/example/miniyoutube/VideoDetailActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.miniyoutube.databinding.ActivityMainBinding
+import com.example.miniyoutube.databinding.ActivityVideoDetailBinding
 
 class VideoDetailActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityVideoDetailBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_video_detail)
-
+        setContentView(binding.root)
     }
 }
